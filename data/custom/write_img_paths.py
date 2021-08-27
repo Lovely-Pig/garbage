96e3cd1fe61f9bdf2cc1b2894f017efcd801452f
Lovely-Pig <1607806186@qq.com>
2021-08-27 19:28:33 +0800

    init

---
+++ data/custom/write_img_paths.py
@@ -0,0 +1,23 @@
+import os
+
+
+def write_train_path(img_name: str):
+    with open('train.txt', 'a') as f:
+        f.write(f'data/custom/images/{img_name}\n')
+
+
+def write_valid_path(img_name: str):
+    with open('valid.txt', 'a') as f:
+        f.write(f'data/custom/images/{img_name}\n')
+
+
+filepath = 'images/'
+filenames = os.listdir(path=filepath)
+
+
+for filename in filenames:
+    write_train_path(filename)
+
+
+for filename in filenames[-10:]:
+    write_valid_path(filename)
---
+++ detect_cam.py
@@ -0,0 +1,271 @@
+#! /usr/bin/env python3
+
+from __future__ import division
+
+import os
+import cv2
+import random
+import numpy as np
+
+import torch
+from torch.autograd import Variable
+
+from models import load_model
+from utils.utils import load_classes
+
+import pickle as pkl
+
+
+
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+    
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes 
+    
+    
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    
+    #Intersection area
+    if torch.cuda.is_available():
+            inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,torch.zeros(inter_rect_x2.shape).cuda())*torch.max(inter_rect_y2 - inter_rect_y1 + 1, torch.zeros(inter_rect_x2.shape).cuda())
+    else:
+            inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,torch.zeros(inter_rect_x2.shape))*torch.max(inter_rect_y2 - inter_rect_y1 + 1, torch.zeros(inter_rect_x2.shape))
+    
+    #Union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    
+    return iou
+
+
+def prep_image(img, inp_dim):
+    """
+    Prepare image for inputting to the neural network. 
+    
+    Returns a Variable 
+    """
+
+    orig_im = img
+    dim = orig_im.shape[1], orig_im.shape[0]
+    img = cv2.resize(orig_im, (inp_dim, inp_dim))
+    img_ = img[:,:,::-1].transpose((2,0,1)).copy()
+    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+    return img_, orig_im, dim
+
+
+def write_results(prediction, confidence, num_classes, nms = True, nms_conf = 0.4):
+    conf_mask = (prediction[:,:,4] > confidence).float().unsqueeze(2)
+    prediction = prediction*conf_mask
+    
+
+    try:
+        ind_nz = torch.nonzero(prediction[:,:,4]).transpose(0,1).contiguous()
+    except:
+        return 0
+    
+    
+    box_a = prediction.new(prediction.shape)
+    box_a[:,:,0] = (prediction[:,:,0] - prediction[:,:,2]/2)
+    box_a[:,:,1] = (prediction[:,:,1] - prediction[:,:,3]/2)
+    box_a[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2) 
+    box_a[:,:,3] = (prediction[:,:,1] + prediction[:,:,3]/2)
+    prediction[:,:,:4] = box_a[:,:,:4]
+    
+
+    
+    batch_size = prediction.size(0)
+    
+    output = prediction.new(1, prediction.size(2) + 1)
+    write = False
+
+
+    for ind in range(batch_size):
+        #select the image from the batch
+        image_pred = prediction[ind]
+        
+
+        
+        #Get the class having maximum score, and the index of that class
+        #Get rid of num_classes softmax scores 
+        #Add the class index and the class score of class having maximum score
+        max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        seq = (image_pred[:,:5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+        
+
+        
+        #Get rid of the zero entries
+        non_zero_ind =  (torch.nonzero(image_pred[:,4]))
+
+        
+        image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
+        
+        #Get the various classes detected in the image
+        try:
+            img_classes = unique(image_pred_[:,-1])
+        except:
+             continue
+        #WE will do NMS classwise
+        for cls in img_classes:
+            #get the detections with one particular class
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+            
+
+            image_pred_class = image_pred_[class_mask_ind].view(-1,7)
+
+		
+        
+             #sort the detections such that the entry with the maximum objectness
+             #confidence is at the top
+            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)
+            
+            #if nms has to be done
+            if nms:
+                #For each detection
+                for i in range(idx):
+                    #Get the IOUs of all boxes that come after the one we are looking at 
+                    #in the loop
+                    try:
+                        ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+                    except ValueError:
+                        break
+        
+                    except IndexError:
+                        break
+                    
+                    #Zero out all the detections that have IoU > treshhold
+                    iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                    image_pred_class[i+1:] *= iou_mask       
+                    
+                    #Remove the non-zero entries
+                    non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                    image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+                    
+                    
+
+            #Concatenate the batch_id of the image to the detection
+            #this helps us identify which image does the detection correspond to 
+            #We use a linear straucture to hold ALL the detections from the batch
+            #the batch_dim is flattened
+            #batch is identified by extra batch column
+            
+            
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+            else:
+                out = torch.cat(seq,1)
+                output = torch.cat((output,out))
+    
+    return output
+
+
+def write(x, img, classes, colors):
+    c1 = x[1:3].int().tolist()
+    c2 = x[3:5].int().tolist()
+    cls = int(x[-1])
+    label = "{0}".format(classes[cls])
+    color = random.choice(colors)
+    cv2.rectangle(img, c1, c2,color, 1)
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+    c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+    cv2.rectangle(img, c1, c2,color, -1)
+    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    return img
+
+
+def run():
+    model = "config/yolov3.cfg"
+    weights = "checkpoints/yolov3_ckpt_30.pth"
+    classes = "data/custom/classes.names"
+    img_size = 416
+    conf_thres  = 0.5
+    nms_thres  = 0.4
+
+    model = load_model(model, weights)
+    
+    # Extract class names from file
+    classes = load_classes(classes)  # List of class names
+    num_classes = len(classes)
+
+    CUDA = torch.cuda.is_available()
+
+    cap = cv2.VideoCapture(0)
+    
+    assert cap.isOpened(), 'Cannot capture source'
+    
+    frames = 0
+    while cap.isOpened():
+        
+        ret, frame = cap.read()
+        if ret:
+            
+            img, orig_im, dim = prep_image(frame, img_size)
+            
+            im_dim = torch.FloatTensor(dim).repeat(1,2)
+
+            if CUDA:
+                im_dim = im_dim.cuda()
+                img = img.cuda()
+            
+            output = model(img)
+            output = write_results(output, conf_thres, num_classes, nms = True, nms_conf = nms_thres)
+
+            if type(output) == int:
+                frames += 1
+                cv2.imshow("frame", orig_im)
+                key = cv2.waitKey(1)
+                if key & 0xFF == ord('q'):
+                    break
+                continue
+            
+            output[:,1:5] = torch.clamp(output[:,1:5], 0.0, float(img_size))/img_size
+            
+            # im_dim = im_dim.repeat(output.size(0), 1)
+            output[:,[1,3]] *= frame.shape[1]
+            output[:,[2,4]] *= frame.shape[0]
+            
+            colors = pkl.load(open("pallete", "rb"))
+            
+            list(map(lambda x: write(x, orig_im, classes, colors), output))
+            
+            
+            cv2.imshow("frame", orig_im)
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q'):
+                break
+            frames += 1
+            
+        else:
+            break
+
+
+if __name__ == '__main__':
+    run()
